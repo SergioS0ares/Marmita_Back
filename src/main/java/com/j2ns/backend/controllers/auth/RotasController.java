4b88dd768ba7fff82a4d4afdfc1aafc854650fd9
Salahil <julioykawahata@gmail.com>
2024-11-30 17:24:09 -0300

    Reparaçoes nas classes de rotas e correçao de logica

--- src/main/java/com/j2ns/backend/controllers/auth/RotasController.java
+++ src/main/java/com/j2ns/backend/controllers/auth/RotasController.java
@@ -1,7 +1,10 @@
 package com.j2ns.backend.controllers.auth;
 
+import com.j2ns.backend.config.Entregador;
+import com.j2ns.backend.config.JSONobjectRotas;
 import com.j2ns.backend.models.auth.RotasModel;
 import com.j2ns.backend.services.auth.RotasService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,21 +17,23 @@ public class RotasController {
     @Autowired
     private RotasService serv;
 
-    // Requisição POST para salvar uma lista de RotasModel
+    // Requisição POST para calcular as rotas com uma lista de RotasModel e dados do Entregador
     @PostMapping("/calcularRotas")
-    public void calcularRotas(@RequestBody List<RotasModel> rotas) {
-        serv.calcularRotas(rotas);
+    public void calcularRotas(@RequestBody List<RotasModel> rotas, @RequestParam int capacidadeMarmitas) {
+        Entregador entregador = new Entregador();
+        entregador.setQuantMarmitaEntregador(capacidadeMarmitas); // Define a capacidade do entregador
+        serv.calcularRotas(rotas, entregador);
     }
 
-    // Requisição GET para obter a lista2 calculada
+    // Requisição GET para obter os destinos e restante das marmitas do entregador
     @GetMapping("/getDestino")
-    public List<RotasModel> getDestino() {
-        return serv.getDestinos();
+    public JSONobjectRotas getDestino() {
+        return serv.getDestinos(); // Retorna o JSONobjectRotas com a lista de destinos e status do entregador
     }
 
-    // Requisição GET para retornar a lista3
+    // Requisição GET para obter a lista final de rotas após o cálculo
     @GetMapping("/getRotas")
     public List<RotasModel> getRotas() {
-        return serv.getRotas();
+        return serv.getRotas(); // Retorna a lista final calculada
     }
 }
